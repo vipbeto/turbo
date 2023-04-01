da94a97fe34b727e757de0b9abb92988e580a311
J.F. Roque <123225396+Cyber-ssh@users.noreply.github.com>
2023-04-01 08:53:50 -0400

    Add files via upload

--- Sistema/compile.sh
+++ Sistema/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tput setaf 7 ; tput setab 4 ; tput bold ; printf '%50s%s%-20s\n' "BadVPN, created By TURBONET-SSH" ; tput sgr0
+tput setaf 7 ; tput setab 4 ; tput bold ; printf '%50s%s%-20s\n' "BadVPN, created By CYBERSSH TURBO" ; tput sgr0
 if [ -f "/usr/local/bin/badvpn-udpgw" ]
 then
 	tput setaf 3 ; tput bold ; echo ""
--- Sistema/open.py
+++ Sistema/open.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-# ULTIMATE By @TURBONETVPN2023
+# ULTIMATE By CYBERSSH TURBO
 import socket, threading, thread, select, signal, sys, time
 from os import system
 system("clear")
